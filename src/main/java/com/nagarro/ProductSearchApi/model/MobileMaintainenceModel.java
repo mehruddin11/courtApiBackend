cdc78a7774753e5af8b59355ec2c9a02b460a951
mehruddin khan <77490569+mehruddin11@users.noreply.github.com>
2024-01-19 17:27:02 +0530

    Update MobileMaintainenceModel.java

--- src/main/java/com/nagarro/ProductSearchApi/model/MobileMaintainenceModel.java
+++ src/main/java/com/nagarro/ProductSearchApi/model/MobileMaintainenceModel.java
@@ -6,7 +6,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-
+// mobile model
 @Entity
 public class MobileMaintainenceModel {
 
