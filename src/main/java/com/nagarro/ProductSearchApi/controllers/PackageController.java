9d14e83f39eba7cf32081147d38a5a2cca193c65
mehruddin khan <77490569+mehruddin11@users.noreply.github.com>
2024-01-19 17:34:11 +0530

    Update PackageController.java

--- src/main/java/com/nagarro/ProductSearchApi/controllers/PackageController.java
+++ src/main/java/com/nagarro/ProductSearchApi/controllers/PackageController.java
@@ -1,11 +1,18 @@
 package com.nagarro.ProductSearchApi.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/package")
 public class PackageController {
+	
+	
+	@GetMapping
+	public String getText() {
+		return "hello";
+	}
 	
 	
 
